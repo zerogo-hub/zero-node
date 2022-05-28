1aca41920b9af1713616c93077415be785f4752e
alex <alex_my@126.com>
2022-05-28 23:03:44 +0800

    ✨ feat(network): 删除心跳消息

--- pkg/network/flag.go
+++ pkg/network/flag.go
@@ -8,10 +8,7 @@ const (
 	// FlagEncrypt 负载 payload 被加密
 	FlagEncrypt = uint16(0x0010)
 
-	// FlagTick 这是心跳包消息
-	FlagTick = uint16(0x0100)
-
 	// FlagInit 这是初始化消息
 	// 用于连接时与客户端做一些初始化信息，例如通过 dh 协议交换密钥，用于后续的 rc4 加密
-	FlagInit = uint16(0x1000)
+	FlagInit = uint16(0x0100)
 )
